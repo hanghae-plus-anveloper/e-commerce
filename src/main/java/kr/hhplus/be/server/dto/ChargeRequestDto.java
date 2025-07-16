3df83a8ad4b84b9b2f3435ef0d2d10e9859d5824
anveloper <hitedin@gmail.com>
2025-07-16 23:40:59 +0900

    feat: 잔액 충전 및 조회 swagger 구현

---
+++ src/main/java/kr/hhplus/be/server/dto/ChargeRequestDto.java
@@ -0,0 +1,17 @@
+package kr.hhplus.be.server.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "잔액 충전 요청 DTO")
+public class ChargeRequestDto {
+    @Schema(description = "충전 금액", example = "10000")
+    @Min(value = 1, message = "충전 금액은 1원 이상이어야 합니다.")
+    private int amount;
+}
