fbec9c8e1701ac2eb90490f71a8f756b781d1dbf
anveloper <hitedin@gmail.com>
2025-07-17 20:35:54 +0900

    feat: 주문 생성 swagger 구현

---
+++ src/main/java/kr/hhplus/be/server/dto/OrderResponseDto.java
@@ -0,0 +1,16 @@
+package kr.hhplus.be.server.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Schema(description = "주문 응답 DTO")
+public class OrderResponseDto {
+    @Schema(description = "주문 ID", example = "1001")
+    private Long orderId;
+
+    @Schema(description = "총 결제 금액", example = "20000")
+    private int totalAmount;
+}
