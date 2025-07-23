ee484e4714560f028e2ff9892d573cd30ba3f642
anveloper <hitedin@gmail.com>
2025-07-23 23:30:13 +0900

    feat: ProductController 서비스 연결

--- src/main/java/kr/hhplus/be/server/controller/product/ProductController.java
+++ src/main/java/kr/hhplus/be/server/controller/product/ProductController.java
@@ -1,32 +1,25 @@
 package kr.hhplus.be.server.controller.product;
 
+import kr.hhplus.be.server.application.product.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class ProductController implements ProductApi {
+
+    private final ProductService productService;
 
     @Override
     public ResponseEntity<List<ProductResponseDto>> getAllProducts() {
-        List<ProductResponseDto> mockProducts = List.of(
-            new ProductResponseDto(1L, "USB-C 충전기", 19900, 25),
-            new ProductResponseDto(2L, "노트북 거치대", 39900, 10)
-        );
-        return ResponseEntity.ok(mockProducts);
+        return ResponseEntity.ok(productService.getAllProducts());
     }
 
     @Override
     public ResponseEntity<ProductResponseDto> getProductById(Long productId) {
-        if (productId == 1L) {
-            ProductResponseDto product = new ProductResponseDto(1L, "USB-C 충전기", 19900, 25);
-            return ResponseEntity.ok(product);
-        } else if (productId == 2L) {
-            ProductResponseDto product = new ProductResponseDto(2L, "노트북 거치대", 39900, 10);
-            return ResponseEntity.ok(product);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return ResponseEntity.ok(productService.getProductById(productId));
     }
 }
