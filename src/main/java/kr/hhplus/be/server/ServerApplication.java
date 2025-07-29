a83fa59ebe83664b3aebfa8a010d1d4160b4a53a
anveloper <hitedin@gmail.com>
2025-07-29 23:26:30 +0900

    refactor: 불필요 코드 제거, 스캔 범위 재수정

--- src/main/java/kr/hhplus/be/server/ServerApplication.java
+++ src/main/java/kr/hhplus/be/server/ServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "kr.hhplus")
-@EnableJpaRepositories(basePackages = "kr.hhplus")
-@EntityScan(basePackages = "kr.hhplus")
+@EnableJpaRepositories(basePackages = "kr.hhplus.be.server")
+@EntityScan(basePackages = "kr.hhplus.be.server")
 public class ServerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/kr/hhplus/be/server/balance/domain/BalanceRepository.java
+++ src/main/java/kr/hhplus/be/server/balance/domain/BalanceRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface BalanceRepository extends JpaRepository<Balance, Long> {
-    Optional<Balance> findByUserId(Long userId);
 }
