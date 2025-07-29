9651481466ba26f1114ca5fa7473e29cfca7ec34
anveloper <hitedin@gmail.com>
2025-07-29 21:55:34 +0900

    fix: 엔티티 구조 변경에 따른 스캔 범위 수정

--- src/main/java/kr/hhplus/be/server/ServerApplication.java
+++ src/main/java/kr/hhplus/be/server/ServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "kr.hhplus")
-@EnableJpaRepositories(basePackages = "kr.hhplus.be.server.domain")
-@EntityScan(basePackages = "kr.hhplus.be.server.domain")
+@EnableJpaRepositories(basePackages = "kr.hhplus")
+@EntityScan(basePackages = "kr.hhplus")
 public class ServerApplication {
 
 	public static void main(String[] args) {
